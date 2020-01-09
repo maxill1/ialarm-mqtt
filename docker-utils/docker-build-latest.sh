686bf3d49e056eeb5640f792717d69827814f371
Luca Mazzilli <setmaat@gmail.com>
2020-01-09 22:49:29 +0100

    docker utils: get package.json version

--- docker-utils/docker-build-latest.sh
+++ docker-utils/docker-build-latest.sh
@@ -1,1 +1,7 @@
-docker buildx build --platform linux/386,linux/amd64,linux/arm64,linux/arm/v7,linux/arm/v6 -t maxill1/ialarm-mqtt:latest -t maxill1/ialarm-mqtt:0.4.0 . --push
+PACKAGE_VERSION=$(cat package.json \
+  | grep version \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g' \
+  | tr -d '[[:space:]]')
+ docker buildx build --platform linux/386,linux/amd64,linux/arm64,linux/arm/v7,linux/arm/v6 -t maxill1/ialarm-mqtt:latest -t maxill1/ialarm-mqtt:$PACKAGE_VERSION . --push
